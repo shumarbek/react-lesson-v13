baac17c9779ff509216be1d40a780fa2613e2236
ShUmarbek <shumarbek001@gmail.com>
2026-05-07 15:36:01 +0500

    fourtth commit

---
+++ src/components/Main.tsx
@@ -0,0 +1,10 @@
+const Main = () => {
+    return (
+        <main>
+            <h1>Welcome to My App</h1>
+            <p>This is the main content area.</p>
+        </main>
+    )
+}
+
+export default Main;
