fab7a1aadb51069f9aa6f95f3104340a50e9fbf2
ShUmarbek <shumarbek001@gmail.com>
2026-05-07 15:41:27 +0500

    ...

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,6 +5,13 @@ const Main = () => {
                 <h1>Welcome to My App</h1>
                 <p>This is the main content area.</p>
             </section>
+
+            <section>
+                <div>
+                    <h1>Youtube video</h1>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/H3FANZwkXpk?si=ekWcW-50k4JhzuG4" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+                </div>
+            </section>
         </main>
     )
 }
