a7ece205616e4231d6afee4028b9f4de426ade5b
ShUmarbek <shumarbek001@gmail.com>
2026-05-07 15:21:44 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import Layout from "./components/Layout";
+
+function App() {
+  return (
+    <StrictMode>
+      <Layout />
+    </StrictMode>
+  )
+}
+
+export default App;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,11 @@
+const Layout = () => {
+    return (
+        <div>
+            <h1>Carusel</h1>
+            <h1>Video</h1>
+            <h1>Pricing</h1>
+        </div>
+    )
+}
+
+export default Layout;
